92280993633bdad0cb2e33de8928bffae615bdde
4idaJV <aida.jv@hotmail.es>
2024-04-23 16:54:43 +0100

    sign up

--- src/layouts/mainLayout.jsx
+++ src/layouts/mainLayout.jsx
@@ -4,8 +4,9 @@ import { Outlet } from 'react-router-dom'
 const Root = () => {
   return (
     <div>
+      {/* <div>Header</div> */}
       <Outlet />
-      <div>Footer</div>
+      {/* <div>Footer</div> */}
     </div>
   )
 }
