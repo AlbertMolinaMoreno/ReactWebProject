f30bc2f8da6f2c7e1848cf4a3c0338cfb97fccd9
AlbertMolinaMoreno <albertomolinamoreno@gmail.com>
2024-04-23 16:41:49 +0100

    dia 2 front

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,27 @@
 import './Home.css'
+import Filtrado from '../../components/Filtrado/Filtrado.jsx'
+import {getTodos} from '../../services/auth.js'
+import {useState,useEffect} from 'react'
 
 const Home = () => {
+  const [recetas,SetRecetas] = useState([]);
+
+  useEffect(()=> {
+    TodasRecetas()
+  },[])
+
+  async function TodasRecetas() {
+    const data = await getTodos()
+    SetRecetas(data)
+  }
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <div className='recetasActu'>
+         <Filtrado  recetas={recetas}/>
+      </div>
+   
+    </div>
   )
 }
 
