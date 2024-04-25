ca1c687cdaca8ce60b8c57c46ccc62f24ad6df44
CynHM <cynhmyanez@gmail.com>
2024-04-25 17:16:11 +0100

    una cosa menos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,32 @@
-// Header.jsx
-import './Header.css';
+import "./Header.css";
+import foto from "../../../public/imagenes/1.png";
+import foto2 from "../../../public/imagenes/cuenta.png";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className='logo'>
-        <img src="file:///home/reboot/Downloads/logo_200x200-removebg-preview.png" alt="Logo" />
+      <div className="logo">
+        <img src={foto} alt="Log" />
       </div>
       <nav className="navigation">
+        <button className="boton">CREATE MENU</button>
         <ul className="nav-list">
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Recetas</a></li>
-          <li><a href="#">Contacto</a></li>
+          <li>
+            <a href="#">RECIPIES</a>
+          </li>
+          <li>
+            <a href="#">ABOUT</a>
+          </li>
+          <li>
+            <a href="#">CONTACT</a>
+          </li>
         </ul>
+        <button className="login">
+          <img src={foto2} alt="Log" />
+        </button>
       </nav>
     </div>
   );
-}
+};
 
 export default Header;
